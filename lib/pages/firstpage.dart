0105a5041f373f477d8422372a2af96809e572e1
simming11 <abdulazem.m13@gmail.com>
2023-12-15 15:23:38 +0700

    80

--- lib/pages/firstpage.dart
+++ lib/pages/firstpage.dart
@@ -17,88 +17,46 @@ class _FirstpageState extends State<Firstpage> {
       appBar: AppBar(
         title: const Text("input widget"),
       ),
-      body: Column(
-        children: [
-          RadioListTile(
-            title: const Text("noodle 50 บาท"),
-            value: "noodle",
-            groupValue: foodradio,
-            onChanged: (value) {
-              setState(() {
-                foodradio = value!;
-              });
-
-              // ignore: avoid_print
-              print(foodradio);
-            },
-          ),
-          RadioListTile(
-            title: const Text("mama 30 บาท"),
-            value: "mama",
-            groupValue: foodradio,
-            onChanged: (value) {
-              setState(() {
-                foodradio = value!;
-              });
-
-              // ignore: avoid_print
-              print(foodradio);
-            },
-          ),
-          RadioListTile(
-            title: const Text("ผัดไก่ 30 บาท"),
-            value: "ผัดไก่",
-            groupValue: foodradio,
-            onChanged: (value) {
-              setState(() {
-                foodradio = value!;
-              });
-
-              // ignore: avoid_print
-              print(foodradio);
-            },
-          ),
-          RadioListTile(
-            title: const Text("ผัดกระเพา 30 บาท"),
-            value: "ผัดกระเพา",
-            groupValue: foodradio,
-            onChanged: (value) {
-              setState(() {
-                foodradio = value!;
-              });
-
-              // ignore: avoid_print
-              print(foodradio);
-            },
-          ),
-          RadioListTile(
-            tileColor: Colors.deepPurple,
-            activeColor: Colors.amber,
-            title: const Text("ผัดแมว 30 บาท"),
-            value: "ผัดแมว",
-            groupValue: foodradio,
-            onChanged: (value) {
-              setState(() {
-                foodradio = value!;
-              });
-
-              // ignore: avoid_print
-              print(foodradio);
-            },
-          ),
-          const Divider(),
-          CheckboxListTile(
-            controlAffinity: ListTileControlAffinity.leading,
-            title: const Text("ไข่ดาว"),
-            value: staregg,
-            onChanged: (value) {
-              setState(() {
-                staregg = value!;
-              });
-            },
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            radioMenulist("แมว","50"),
+            radioMenulist("แมว","50"),
+            radioMenulist("แมว","50"),
+            radioMenulist("แมว","50"),
+            radioMenulist("แมว","50"),
+            const Divider(),
+            CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              secondary: const Text("25"),
+              title: const Text("ไข่ดาว"),
+              value: staregg,
+              onChanged: (value) {
+                setState(() {
+                  staregg = value!;
+                });
+              },
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  RadioListTile<String> radioMenulist(String title, String subtitle) {
+    return RadioListTile(
+      title: Text(title),
+      value: title,
+      subtitle: Text(subtitle),
+      groupValue: foodradio,
+      onChanged: (value) {
+        setState(() {
+          foodradio = value!;
+        });
+
+        // ignore: avoid_print
+        print(foodradio);
+      },
     );
   }
 }
